08316bad3213d7ba1550e657e63fba4da6f000c2
arfat-xyz <arfatrahman08@gmail.com>
2023-08-01 22:25:13 +0600

    new url added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "apidata",
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:5000/api/v1/`,
+    baseUrl: `https://bookshop-backend.vercel.app/api/v1/`,
   }),
   tagTypes: ["readlist", "wishlish", "updatebook"],
   endpoints: (builder) => ({
